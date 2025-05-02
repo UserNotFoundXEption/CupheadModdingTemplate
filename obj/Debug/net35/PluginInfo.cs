71275858f221e8faa75847b00add00228dfe7b4a
Kamil <01178868@pw.edu.pl>
2025-05-02 18:22:59 +0200

    removed unwanted folder

--- obj/Debug/net35/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace CupheadModdingTemplate
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "CupheadModdingTemplate";
-        public const string PLUGIN_NAME = "CupheadModdingTemplate";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
